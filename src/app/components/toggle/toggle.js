1d9ffd8f5d0d1cb201035dd5ee3cfec12e82d255
yurikrupnik <krupnik.yuri@gmail.com>
2016-03-12 18:55:11 +0200

    added splitByDot

--- src/app/components/toggle/toggle.js
+++ src/app/components/toggle/toggle.js
@@ -12,24 +12,30 @@
     var isBoolean =  lodash.isBoolean;
     var reduce = lodash.reduce;
     var split = lodash.split;
+    var trim = lodash.trim;
 
-    function toggleBool(obj, nextProp) {
+    function splitByDot(string) {
+      return split(string, '.');
+    }
+    function toggleBool(obj, currentValue) {
       // toggles bool property when is passes the iteer check
       // else returns new value for next iteer
+      var nextProp = trim(currentValue);
       if (isBoolean(obj[nextProp])) {
-        obj[nextProp] = !obj[nextProp];
+        obj[nextProp] = !obj[nextProp]; // this line is the brain of the directive = all that code for dynamic toggle on click directive
         return false;
       }
       return obj[nextProp];
     }
 
-    function toggleStateOnObject(initialState, string) {
-      var props = split(string, '.');
+    function toggleStateOnObject(initialState, stringOfProps) {
+      var props = splitByDot(stringOfProps);
       if (props.length <= 1) {
         // make sure u use vm
         $log.warn('not using vm');
 
       }
+      // todo name it, make a func for it
       reduce(props, toggleBool, initialState);
     }
 
